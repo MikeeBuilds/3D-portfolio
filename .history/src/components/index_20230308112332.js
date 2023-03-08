e8cb82af0111edbbe8355fd56fb8aa82959f938d
Algo Hussle <citizensofandrometta@gmail.com>
2023-03-08 11:24:02 -0600

    footer added

--- .history/src/components/index_20230308112332.js
+++ .history/src/components/index_20230308112332.js
@@ -7,7 +7,7 @@ import Experience from './Experience';
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
-import Footer from './Footer';
+
 
 export {
   Hero,
