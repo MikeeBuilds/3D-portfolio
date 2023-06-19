eebbbb104d71aebd7e9523b43606413b20be0250
Algo Hussle <citizensofandrometta@gmail.com>
2023-06-18 23:32:55 -0500

    fixed hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,50 +5,36 @@ import { ComputersCanvas } from './canvas';
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'
-    >
+    <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col items-center justify-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
-          </div>
-          <div className=''> 
-            <h1 className={`${styles.heroHeadText} text-white`}> 
+        </div>
+        <div className=''> 
+          <h1 className={`${styles.heroHeadText} text-white`}> 
             GM! Im <span className=' text-[#915eff]'>Algo</span>
-            </h1>
-            <p className=''>
-                Currently building...  
-            </p>
-            <p className={`${styles.heroSubText}  mt-2 text-white-100`}>
-<<<<<<< HEAD
-            <span className=' animate-text  bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-4xl font-black xs:flex-row '>Umbrella Labs
-=======
-            <span className=' animate-text  bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-4xl font-black xs:flex-row '>Umbr3lla Labs
->>>>>>> c337d917947eef7343d1586677402648fc4e3252
-</span> Where Development Meets Innovation
-
-
-            </p>
-          </div>
+          </h1>
+          <p className=''>
+            Currently building...  
+          </p>
+          <p className={`${styles.heroSubText}  mt-2 text-white-100`}>
+            <span className=' animate-text  bg-gradient-to-r from-teal-500 via-purple-500 to-orange-500 bg-clip-text text-transparent text-4xl font-black xs:flex-row '>Umbr3lla Labs</span> Where Development Meets Innovation
+          </p>
+        </div>
       </div>
-            <ComputersCanvas />
-            <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center
-            items-center'>
-              <a href="#about">
-                <div className='w-35-[35px] h-[64px] rounded-3xl border-4 border-secondary flex items-center
-                p-2'>
-                  <motion.div 
-                  animate={{ y: [0, 24, 0] }}
-                  transition={{ duration: 1.5,
-                                repeat: Infinity,
-                                repeatType: 'loop' 
-                              }}
-                  className='w-3 h-3 rounded-full mb-1 bg-secondary'
-                  
-                  />
-                </div>
-              </a>
-            </div>
+      <ComputersCanvas />
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href="#about">
+          <div className='w-35-[35px] h-[64px] rounded-3xl border-4 border-secondary flex items-center p-2'>
+            <motion.div 
+              animate={{ y: [0, 24, 0] }}
+              transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }}
+              className='w-3 h-3 rounded-full mb-1 bg-secondary'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
